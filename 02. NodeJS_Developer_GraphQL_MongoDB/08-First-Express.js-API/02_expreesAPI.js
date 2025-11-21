0a9937bf7cf6b3b141568dc678f6368ec5aa87a5
Juwel-771 <juwelah99@gmail.com>
2025-11-21 17:12:47 +0600

    Data get API route express js

--- 02. NodeJS_Developer_GraphQL_MongoDB/08-First-Express.js-API/02_expreesAPI.js
+++ 02. NodeJS_Developer_GraphQL_MongoDB/08-First-Express.js-API/02_expreesAPI.js
@@ -3,12 +3,37 @@ const express = require('express');
 const app = express();
 const PORT= 6100;
 
-app.get('/', (req, res)=>{
-    res.send({
+const friends = [
+    {
         id: 1,
-        name: "Meherab"
-    });
+        name: 'Meherab'
+    },
+    {
+        id: 2,
+        name: "Juwel"
+    },
+    {
+        id: 3,
+        name: "Michael"
+    }
+]
+
+app.get('/friends', (req, res)=>{
+    res.json(friends);
 });
+
+app.get('/friends/:friendId', (req, res)=>{
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    
+    if(friend){
+        res.status(200).json(friend);
+    }else {
+        res.status(404).json({
+            err: "Friend ID not found"
+        })
+    }
+})
 
 app.get('/message', (req, res)=>{
     res.send("<ul><li>Greetings! </li></ul>")
