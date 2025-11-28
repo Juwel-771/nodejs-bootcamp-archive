3b98ef8f18eec8d48c28995276d78f54bebde74c
Juwel-771 <juwelah99@gmail.com>
2025-11-28 06:17:27 +0600

    static css style added

--- 01. Code_step_by_step/Express/12_form.js
+++ 01. Code_step_by_step/Express/12_form.js
@@ -8,6 +8,11 @@ import path from "path";
 
 const app = express();
 
+const publicPath = path.resolve('public');
+
+app.use(express.static(publicPath));
+console.log(publicPath);
+
 app.get("/", (req, res)=>{
     res.send(home());
 });
