4c0c5a61fb1a41bf1e832bd2b8771a68de83b06f
Juwel-771 <juwelah99@gmail.com>
2025-12-06 03:02:31 +0600

    route added

--- 04. Yahu/index.js
+++ 04. Yahu/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
 const app = express();
 
 app.get("/", (req, res)=>{
     res.send("<h1>Hello World</h1>");
+})
+
+app.get("/about", (req, res)=>{
+    res.send("New about page");
+})
+
+app.get("/user/:userId/book/:bookId", (req, res)=>{
+    res.send(req.params);
 })
 
 app.listen(5000, ()=>{
