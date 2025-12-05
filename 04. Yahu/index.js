c6011082ffaeeb7e0bd68ed0e9e119474cb0d7b1
Juwel-771 <juwelah99@gmail.com>
2025-12-06 03:23:40 +0600

    router added

--- 04. Yahu/index.js
+++ 04. Yahu/index.js
@@ -9,8 +9,16 @@ app.get("/about", (req, res)=>{
     res.send("New about page");
 })
 
-app.get("/user/:userId/book/:bookId", (req, res)=>{
+app.get("/user/:userId-:bookId", (req, res)=>{
     res.send(req.params);
+})
+
+app.get("/search", (req, res)=>{
+
+    const name = req.query.name;
+    const age = req.query.age;
+
+    res.send(`Searched value for Name :${name}, Age: ${age}`);    
 })
 
 app.listen(5000, ()=>{
